c9fbc73ac2ce18396a37f67b227b05ba63cacfe4
Rohan Yadav <yrohan645@gmail.com>
2026-03-04 19:53:02 +0530

    second  commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import EmployeeManagement from "./components/EmployeeManagement";
-import Display from "./pages/Display";
-import Add from "./components/Add";
-import Edit from "./components/Edit";
-import Delete from "./components/Delete";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import AdminLogin from "./components/AdminLogin";
@@ -24,7 +19,7 @@ function App() {
             <ProtectedRoute>
               <AdminDashboard />
             </ProtectedRoute>
-          }
+          }   
         />
 
       </Routes>
