9477a63dab9e769ae8d5e1f55447a61063c71517
Rohan Yadav <yrohan645@gmail.com>
2026-03-04 21:23:04 +0530

    second  commit

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from "react-router";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
-serviceWorkerRegistration.register();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+    <App /> 
     </BrowserRouter>
     
   </React.StrictMode>
 );
-
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
